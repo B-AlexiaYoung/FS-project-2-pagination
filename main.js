0b9e5c2b85fabc5bd5e49ed31c0836dc4249cfd2
B-AlexiaYoung <brlxyng@gmail.com>
2018-07-28 14:53:07 -0600

    displayed pagination based on context

--- main.js
+++ main.js
@@ -1,131 +1,127 @@
 //variables
-let getListItems =[];
-getListItems= $(".student-item");
-console.log(getListItems);
+let getListItems = [];
+getListItems = $(".student-item");
 let pages = [];
 let pgNum = 0;
-currentPage = 1;
+let currentPage = 1;
+let searchTerm = "";
+let counter = 0;
+let noResults = document.getElementsByTagName("h2");
+let getStudents = $(".student-item");
 
-// varibles to figure number of pages for the pagination
+
+// variables to figure out number of pages for the pagination
 const dividePagesBy = 10;
-const paginate = Math.ceil(getListItems.length / dividePagesBy);
+let paginate = Math.ceil(getListItems.length / dividePagesBy);
+// function that displays pagination 
+const displayPagination = (paginate) => {
+    $(".pagination li").remove();
 
-//
-/*const getPages = () =>{
-    for (let i=0; i<getListItems.length; i+=dividePagesBy){
-        pages.push(getListItems.slice(i, i+dividePagesBy));
-        
-            console.log(pages);
-
+    if (currentPage > 1) {
+        $(".pagination").append("<li ><a href = '#'>" + "<" + "</a></li>");
     }
-    return pages;
-   
-
-}
-getPages();*/
-//pagination
-//$(".pagination").append("<li><a href = 'javascript:void(0)'>1</a></li>");
-const displayPagination = () =>{
-    $(".pagination").append("<li><a href = '#'>" + "<" + "</a></li>");
-    for(j =1; j<=paginate; j++){
-$(".pagination").append("<li><a href = '#'>" + j + "</a></li>");
+    for (j = 1; j <= paginate; j++) {
+        $(".pagination").append("<li><a id=list" + j + " href = '#'>" + j + "</a></li>");
     }
-    $(".pagination").append("<li><a href = '#'>" + ">" + "</a></li>");
-
+    if (currentPage < paginate) {
+        $(".pagination").append("<li><a href = '#'>" + ">" + "</a></li>");
+    }
+    if (paginate != 0) {
+        let getCurrentPg = document.getElementById("list" + currentPage);
+        getCurrentPg.classList.add("active");
+    }
 }
 
-//function to display first 10 people.
+//function to display a page of people.
 // .eq() Reduce the set of matched elements to the one at the specified index.
-const showPages = (pgNum) => {
-//$(".student-item") .show(); 
-$(".student-item") .hide();
-for(let i=pgNum; i<=((dividePagesBy*currentPage)-1); i++){
-    $(".student-item").eq(i).show();
-}  
+const showPages = (pgNum, getstudents) => {
+    getStudents.hide();
+    for (let i = pgNum; i <= ((dividePagesBy * currentPage) - 1); i++) {
+        getStudents.eq(i).show();
+    }
+    displayPagination(paginate);
 }
-showPages(pgNum);
+showPages(pgNum, getStudents);
 
+const studentPaginate = () => {
+    // set variables for function
+    let newPage = parseInt(event.target.textContent);
+
+    let pgMove = (event.target.textContent);
+    if (isNaN(newPage)) {
+        newPage = 1;
+    }
+    //  previous and back li controls in pagination
+    if (pgMove === "<") {
+        newPage = currentPage - 1;
+    } else if (pgMove === ">") {
+        newPage = currentPage + 1;
+    }
+
+    // controls pagination functionality:
+    if (newPage !== currentPage) {
+        let getCurrentPg = document.getElementById("list" + currentPage);
+        getCurrentPg.classList.remove("active");
+        pgNum = (newPage * dividePagesBy) - dividePagesBy;
+        currentPage = newPage;
+        getCurrentPg = document.getElementById("list" + currentPage);
+        getCurrentPg.classList.add("active");
+        showPages(pgNum, getStudents);
+    }
+}
 // ===================================
 //          Event Listeners
 //====================================
 
-
-
 // add event listener on pagination div
 const listen = $(".pagination");
 listen[0].addEventListener("click", (event) => {
-    console.log(event.target.textContent);
-
-// set variables for function
-   let calculatePage= parseInt(event.target.textContent);
-   let goToPage = (calculatePage * dividePagesBy)-dividePagesBy;
-   let pgMove = (event.target.textContent);
-
-// set intital  prveious and back  li controls in pagination
-   if (pgMove === "<"){
-       pgMove= -1
-   }else if (pgMove=== ">"){
-       pgMove=1;
-   }
-   console.log(calculatePage);
-   console.log(currentPage);
-
-// controls pagination functionality:
-// 1) previous number 2) next numbers 3)previous symbol 4) next symbol
-   if (calculatePage !== currentPage){
-       //console.log("hi");
-       if(calculatePage < currentPage){
-        pgNum = goToPage;
-        currentPage=calculatePage;
-        //console.log(pgNum);
-        //console.log(currentPage);
-        showPages(pgNum);
-       } else if(calculatePage > currentPage){
-        pgNum = goToPage;
-        currentPage=calculatePage;
-        //console.log(pgNum);
-        //console.log(currentPage);
-        //console.log(goToPage);
-        showPages(pgNum);
-        } else if( pgMove + currentPage < currentPage) {
-            //console.log("waht");
-        currentPage = pgMove + currentPage;
-        pgNum = (currentPage * dividePagesBy) - dividePagesBy;
-        //console.log(pgNum);
-        //console.log(currentPage);
-        showPages(pgNum);
-        }else if( pgMove + currentPage > currentPage) {
-            //console.log("waht");
-        currentPage = pgMove + currentPage;
-        pgNum = (currentPage * dividePagesBy) - dividePagesBy;
-        //console.log(pgNum);
-        //console.log(currentPage);
-        showPages(pgNum);
-        }
-
-   }
+    studentPaginate();
 });
 
+displayPagination(paginate);
 
-
-displayPagination();   
-
-
-// Event listener on Search Box
+// Event listener on Search Box 
+//get search criteria and lowercase all display match.
 const search = $("#search-button");
-search[0].addEventListener("click", (event) =>{
-    // event.preventDefault();
-    let match;
-    let searchTerm = document.getElementById("search").value.toLowerCase();
-    console.log(searchTerm);
+search[0].addEventListener("click", (event) => {
+    $(".query").remove();
+    noResults[0].textContent = ("Students");
+
+    searchTerm = document.getElementById("search").value.toLowerCase();
+    pgNum = 0;
+    currentPage = 1;
     getListItems.hide();
-    for (k=0; k<getListItems.length; k++){
-         let nameText = getListItems[k].getElementsByTagName("h3")[0].innerText.toLowerCase();
-         let email = getListItems[k].getElementsByTagName("span")[0].innerText.toLowerCase();
-         console.log(nameText);
-    if(nameText.indexOf(searchTerm) != -1 || email.indexOf(searchTerm) != -1){
-     getListItems[k].style.display="block";
-     
+    counter = 0;
+    for (k = 0; k < getListItems.length; k++) {
+        let nameText = getListItems[k].getElementsByTagName("h3")[0].innerText.toLowerCase();
+        let email = getListItems[k].getElementsByTagName("span")[0].innerText.toLowerCase();
+        if (nameText.indexOf(searchTerm) != -1 || email.indexOf(searchTerm) != -1) {
+            counter++;
+            getListItems[k].classList.add("query");
+        }
+    } // end for k loop 
+    getStudents = $(".query");
+    let queryResults = $(".query");
+    paginate = Math.ceil(queryResults.length / dividePagesBy);
+    showPages(pgNum, getStudents);
+
+    if (counter === 0) {
+        noResults[0].textContent = ("Sorry, no results were found")
     };
-    } 
-});     
+}); // endlistener on search button
+
+//listener on search field  contains reset
+const searchBox = document.getElementById("search");
+searchBox.addEventListener("focus", (event) => {
+    if (searchBox.value != "") {
+        paginate = Math.ceil(getListItems.length / dividePagesBy);
+        pgNum = 0;
+        currentPage = 1;
+        getStudents = $(".student-item");
+        showPages(pgNum, getStudents);
+        studentPaginate();
+        searchTerm = "";
+        noResults[0].textContent = ("Students");
+    }
+})
